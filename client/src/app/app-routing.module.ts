e18aff342df30cf299253413e6d79d557dbb1146
keith <xqk559@gmail.com>
2020-09-09 11:23:44 -0400

    Added shop module, route, and exportred to app.module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'shop', component: ShopComponent, data: {breadcrumb: 'Home'}},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
