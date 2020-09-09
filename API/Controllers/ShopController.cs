c909d1896568dacf3b3976753733419f98c48a06
keith <xqk559@gmail.com>
2020-09-09 13:28:09 -0400

    Made scc.db and products table from storecontext

--- API/Controllers/ShopController.cs
+++ API/Controllers/ShopController.cs
@@ -1,17 +1,24 @@
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("")]
-    public class ShopController : ControllerBase
+  [ApiController]
+  [Route("")]
+  public class ShopController : ControllerBase
+  {
+    private readonly StoreContext _context;
+    public ShopController(StoreContext context)
     {
-        [HttpGet("shop")]
-        public string ReturnString()
-        {
-            return "shop";
-        }
-        public static string Test { get; set; } = "test";
+      _context = context;
     }
+
+    [HttpGet("shop")]
+    public string ReturnString()
+    {
+      return "shop";
+    }
+    public static string Test { get; set; } = "test";
+  }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,37 +14,38 @@ using Microsoft.Extensions.Logging;
 
 namespace staycoolcharcuterie
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly IConfiguration _config;
+    public Startup(IConfiguration config)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      _config = config;
     }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+      services.AddDbContext<StoreContext>(x =>
+          x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
